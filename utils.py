a07712e3c30881a7b28179069af3db094298b2fb
seahunter4 <641375245@qq.com>
2021-03-16 20:03:05 +0800

    fine tune

--- utils.py
+++ utils.py
@@ -7,6 +7,14 @@ import os.path as osp
 import torch
 from torch.utils.data import TensorDataset
 
+def mkdir_if_missing(directory):
+    if not osp.exists(directory):
+        try:
+            os.makedirs(directory)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
 class Logger(object):
     """
     Write console output to external text file.
